c3cc530a994c1ea76bc713c857c03e61be9e9450
Alvek <cj9ddrd@gmail.com>
2023-06-10 17:21:26 +0300

    1.07 Attempt to fix text clip on warning

--- DistantWorlds.Types/GraphicsHelper.cs
+++ DistantWorlds.Types/GraphicsHelper.cs
@@ -12,11 +12,71 @@ using System.Drawing.Imaging;
 using System.Drawing.Text;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace DistantWorlds.Types
 {
     public static class GraphicsHelper
     {
+        enum TernaryRasterOperations : uint
+        {
+            /// <summary>dest = source</summary>
+            SRCCOPY = 0x00CC0020,
+            /// <summary>dest = source OR dest</summary>
+            SRCPAINT = 0x00EE0086,
+            /// <summary>dest = source AND dest</summary>
+            SRCAND = 0x008800C6,
+            /// <summary>dest = source XOR dest</summary>
+            SRCINVERT = 0x00660046,
+            /// <summary>dest = source AND (NOT dest)</summary>
+            SRCERASE = 0x00440328,
+            /// <summary>dest = (NOT source)</summary>
+            NOTSRCCOPY = 0x00330008,
+            /// <summary>dest = (NOT src) AND (NOT dest)</summary>
+            NOTSRCERASE = 0x001100A6,
+            /// <summary>dest = (source AND pattern)</summary>
+            MERGECOPY = 0x00C000CA,
+            /// <summary>dest = (NOT source) OR dest</summary>
+            MERGEPAINT = 0x00BB0226,
+            /// <summary>dest = pattern</summary>
+            PATCOPY = 0x00F00021,
+            /// <summary>dest = DPSnoo</summary>
+            PATPAINT = 0x00FB0A09,
+            /// <summary>dest = pattern XOR dest</summary>
+            PATINVERT = 0x005A0049,
+            /// <summary>dest = (NOT dest)</summary>
+            DSTINVERT = 0x00550009,
+            /// <summary>dest = BLACK</summary>
+            BLACKNESS = 0x00000042,
+            /// <summary>dest = WHITE</summary>
+            WHITENESS = 0x00FF0062,
+            /// <summary>
+            /// Capture window as seen on screen.  This includes layered windows 
+            /// such as WPF windows with AllowsTransparency="true"
+            /// </summary>
+            CAPTUREBLT = 0x40000000
+        }
+        [DllImport("gdi32.dll", EntryPoint = "BitBlt", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool BitBlt([In] IntPtr hdc, int nXDest, int nYDest, int nWidth, int nHeight, [In] IntPtr hdcSrc, int nXSrc, int nYSrc, TernaryRasterOperations dwRop);
+
+        public static Bitmap CopyGraphicsContent(Graphics source, Rectangle rect)
+        {
+            Bitmap bmp = new Bitmap(rect.Width, rect.Height);
+
+            using (Graphics dest = Graphics.FromImage(bmp))
+            {
+                IntPtr hdcSource = source.GetHdc();
+                IntPtr hdcDest = dest.GetHdc();
+
+                BitBlt(hdcDest, 0, 0, rect.Width, rect.Height, hdcSource, rect.X, rect.Y, TernaryRasterOperations.SRCCOPY);
+
+                source.ReleaseHdc(hdcSource);
+                dest.ReleaseHdc(hdcDest);
+            }
+
+            return bmp;
+        }
         public static bool ContainsColor(List<Color> colors, Color color) => GraphicsHelper.IndexOfColor(colors, color) >= 0;
 
         public static int IndexOfColor(List<Color> colors, Color color)
@@ -518,13 +578,31 @@ namespace DistantWorlds.Types
         {
             if (maxSize != SizeF.Empty)
             {
+                bool flag = false;
                 location = new Point(location.X + 1, location.Y + 1);
                 RectangleF layoutRectangle = new RectangleF((float)location.X, (float)location.Y, maxSize.Width, maxSize.Height);
                 StringFormat genericTypographic = StringFormat.GenericTypographic;
+                //genericTypographic.Trimming = StringTrimming.None;
+                //var test2 = new Region();
+                //test2.MakeInfinite();
+                //graphics.Clip = test2;
                 graphics.DrawString(text, font, dropShadowBrush, layoutRectangle, genericTypographic);
+                if (flag)
+                {
+                    Rectangle rect = new Rectangle((int)layoutRectangle.X, (int)layoutRectangle.Y, (int)layoutRectangle.Width, (int)layoutRectangle.Height);
+                    var bitmp = CopyGraphicsContent(graphics, rect);
+                    bitmp.Save(@"H:\test1.bmp", ImageFormat.Bmp);
+                }
                 location = new Point(location.X - 1, location.Y - 1);
                 layoutRectangle = new RectangleF((float)location.X, (float)location.Y, maxSize.Width, maxSize.Height);
                 graphics.DrawString(text, font, brush, layoutRectangle, genericTypographic);
+
+                if (flag)
+                {
+                    Rectangle rect = new Rectangle((int)layoutRectangle.X, (int)layoutRectangle.Y, (int)layoutRectangle.Width , (int)layoutRectangle.Height);
+                    var bitmp = CopyGraphicsContent(graphics, rect);
+                    bitmp.Save(@"H:\test2.bmp", ImageFormat.Bmp);
+                }
             }
             else
             {
