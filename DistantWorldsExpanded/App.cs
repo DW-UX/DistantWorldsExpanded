2ee5e9a514ee8380685dec71ca5cb8595f232ebd
Alvek <cj9ddrd@gmail.com>
2025-04-13 19:33:26 +0300

    Version change

--- DistantWorldsExpanded/App.cs
+++ DistantWorldsExpanded/App.cs
@@ -8,6 +8,6 @@ namespace DistantWorlds
 {
     static class App
     {
-        public static string ExpansionModVersion = "2.0.0.5";
+        public static string ExpansionModVersion = "2.0.0.6";
     }
 }
