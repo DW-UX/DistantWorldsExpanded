fd0353aeadcf2e213535fee63333d64405bba42a
Alvek <cj9ddrd@gmail.com>
2025-09-05 19:47:32 +0300

    Work in progress

--- DistantWorldsExpanded/DistantWorlds.Controls/Controls/EmpirePolicyScreen.cs
+++ DistantWorldsExpanded/DistantWorlds.Controls/Controls/EmpirePolicyScreen.cs
@@ -14,36 +14,14 @@ namespace DistantWorlds.Controls
     public partial class EmpirePolicyScreen : ScreenPanel
     {
         private Font _font2;
-        public EmpirePolicyScreen(Font font2)
+        private Font _font6;
+        private BuiltObjectImageCache builtObjectImageCache_0;
+        public EmpirePolicyScreen(Font font2, Font font6, BuiltObjectImageCache builtObjectImageCache_0)
         {
             InitializeComponent();
-            _font2 = font2;
-
-            lblDiplomacyTreaties.Text = TextResolver.GetText("Diplomacy - Treaties");
-            lblDiplomacyTreaties.Font = _font2;
-            cmbDiplomacyAuto.Items.AddRange(TextResolver.GetText("Control manually"), TextResolver.GetText("Suggest new treaties"), TextResolver.GetText("Fully automate"));
-
-            lblFreeTradeAgrPriority.Text = TextResolver.GetText("Free Trade Agreement Priority");
-            cmbFreeTradeAgrPriority.Items.AddRange(TextResolver.GetText("Low"), TextResolver.GetText("Normal"), TextResolver.GetText("High"), TextResolver.GetText("Very High"));
-
-            lblDefencePact.Text = TextResolver.GetText("Mutual Defense Pact Priority");
-            cmbDefencePact.Items.AddRange(TextResolver.GetText("Low"), TextResolver.GetText("Normal"), TextResolver.GetText("High"), TextResolver.GetText("Very High"));
-
-            lblBreakTreatie.Text = TextResolver.GetText("Willingness to Break Treaties");
-            cmbBreakTreatie.Items.AddRange(TextResolver.GetText("Low"), TextResolver.GetText("Normal"), TextResolver.GetText("High"), TextResolver.GetText("Very High"));
-
-            lblDiploWarTrade.Text = TextResolver.GetText("Diplomacy - War and Trade Sanctions");
-            lblDiploWarTrade.Font = _font2;
-            cmbDiploWarTrade.Items.AddRange(TextResolver.GetText("Control manually"), TextResolver.GetText("Suggest war and trade sanctions"), TextResolver.GetText("Fully automate"));
-
-            lblUseBlockades.Text = TextResolver.GetText("Use Blockades when have Trade Sanctions against an empire");
-            chkUseBlockades.Text = TextResolver.GetText("DiplomacyTradeSanctionsUseBlockades");
-
-            lblSubjugation.Text = TextResolver.GetText("Subjugation Priority");
-            cmbSubjugation.Items.AddRange(TextResolver.GetText("Low"), TextResolver.GetText("Normal"), TextResolver.GetText("High"), TextResolver.GetText("Very High"));
-
-            lblWarWilingnes.Text = TextResolver.GetText("Willingness to Go To War");
-            cmbWarWilingnes.Items.AddRange(TextResolver.GetText("Low"), TextResolver.GetText("Normal"), TextResolver.GetText("High"), TextResolver.GetText("Very High"));
+            this._font2 = font2;
+            this._font6 = font6;
+            this.builtObjectImageCache_0 = builtObjectImageCache_0;
         }
     }
 }
